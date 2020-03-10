f171c684c7e61ad9ddd8e27a6b4dd1cc6db2ca40
Zahra Mansoori <zahramnsoori@yahoo.com>
2020-03-10 14:32:41 +0330

    9

--- resources/views/admin/book/form.blade.php
+++ resources/views/admin/book/form.blade.php
@@ -5,24 +5,24 @@
             {{ csrf_field() }}
             <div class="form-group">
                 <label for="name">نام کتاب</label>
-                <input type="text" class="form-control" id="name" name="name" value="{{old('name',$bookItem->name)}}">
+                <input type="text" class="form-control" id="name" name="name" value="{{old('name',isset($bookItem)?$bookItem->name:'')}}">
             </div>
             <div class="form-group">
                 <label for="author">نویسنده</label>
                 <input type="text" class="form-control" id="author" name="author"
-                       value="{{old('author',$bookItem->author)}}">
+                       value="{{old('author',isset($bookItem)?$bookItem->author:'')}}">
             </div>
             <div class="form-group">
                 <label for="pub_year">سال انشار</label>
-                <input type="text" class="form-control" id="pub_year" name="pub_year" value="{{old('pub_year',$bookItem->pub_year)}}">
+                <input type="text" class="form-control" id="pub_year" name="pub_year" value="{{old('pub_year',isset($bookItem)?$bookItem->pub_year:'')}}">
             </div>
             <div class="form-group">
                 <label for="pub_name">ناشر</label>
-                <input type="text" class="form-control" id="pub_name" name="pub_name" value="{{old('pub_name',$bookItem->pub_name)}}">
+                <input type="text" class="form-control" id="pub_name" name="pub_name" value="{{old('pub_name',isset($bookItem)?$bookItem->pub_name:'')}}">
             </div>
             <div class="form-group">
                 <label for="translator_name">نام مترجم</label>
-                <input type="text" class="form-control" id="translator_name" name="translator_name" value="{{old('translator_name',$bookItem->translator_name)}}">
+                <input type="text" class="form-control" id="translator_name" name="translator_name" value="{{old('translator_name',isset($bookItem)?$bookItem->translator_name:'')}}">
             </div>
             <br/>
             <div class=" form-group float-right">
@@ -38,7 +38,7 @@
             <br/>
             <div class="form-group ">
                 <label for="num">تعداد موجود</label>
-                <input type="text" class="form-control" id="num" name="num" value="{{old('num',$bookItem->num)}}">
+                <input type="text" class="form-control" id="num" name="num" value="{{old('num',isset($bookItem)?$bookItem->num:'')}}">
             </div>
             <input type="submit" class="btn btn-outline-success float-right" value="ذخیره اطلاعات">
 
